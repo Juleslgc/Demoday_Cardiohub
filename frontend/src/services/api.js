c2e20103184a2ccdb2442127549e872d69c258c4
cmeynet <clemence.meynet14@gmail.com>
2025-10-29 14:30:33 +0100

    Local backup before pull

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -19,7 +19,7 @@ const jwtDecode = require("jwt-decode");
 
 
 // Base API endpoint
-const API_URL = "https://gap-anthony-kissing-legs.trycloudflare.com/api";
+const API_URL = "https://expressions-scotia-cnet-newsletter.trycloudflare.com/api";
 
 // Generic API request handler
 export async function apiRequest(endpoint, method = "GET", body = null, showAlert = false, token = null) {
