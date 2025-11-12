14117f75708344ba5844bfdcc103b229ef2360a6
cmeynet <clemence.meynet14@gmail.com>
2025-11-12 11:31:44 +0100

    fix : change api URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -31,7 +31,7 @@ const jwtDecode = require("jwt-decode");
 // ------------------------------------------------------
 // Base API Endpoint
 // ------------------------------------------------------
-const API_URL = "https://thereby-aqua-adam-glasgow.trycloudflare.com/api";
+const API_URL = "https://scenes-contest-equally-parties.trycloudflare.com/api";
 
 // ------------------------------------------------------
 // Generic API Request Handler
